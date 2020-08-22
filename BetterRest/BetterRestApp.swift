df9f07adef7a8b2d381bb805c24bbce4812ecabb
GogoBear <102480361+GogoBear@users.noreply.github.com>
2020-08-22 21:38:35 +0000

    doc: translate po

--- BetterRest/BetterRestApp.swift
+++ BetterRest/BetterRestApp.swift
@@ -5,3 +5,14 @@
 //
 //  Created by Kris Laratta on 10/8/22.
 //
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
