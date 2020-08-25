439acad714326728e2f0a5ef255a539f1854965b
GogoBear <102480361+GogoBear@users.noreply.github.com>
2020-08-25 14:44:05 +0000

    Update thanks.html

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -50,3 +50,22 @@ struct ContentView: View {
                 Text(alertMessage)
             }
         }
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
